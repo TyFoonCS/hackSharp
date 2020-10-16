e1dea957a21dd05557a78cfcd32c6407af826076
Timur Galeev <templay3000@gmail.com>
2020-10-16 22:42:37 +0500

    Устранение недочетов авторизации/регистрации

--- app.py
+++ app.py
@@ -3,6 +3,29 @@ import sqlite3
 import json
 
 app = flask.Flask(__name__)
+app.secret_key = "P@ssw0rd"
+
+
+def session_render_index():
+    lk = 'Личный кабинет'
+    lka = '/'
+    reg = ''
+    rega = ''
+    enter = 'Выйти'
+    entera = "/exit"
+    return flask.render_template("index.html", login=flask.session["user"], enter=enter, entera=entera,
+                                 rega=rega, reg=reg, lk=lk, lka=lka)
+
+
+def anonim_render_index():
+    lk = ""
+    lka = ""
+    enter = 'Войти'
+    entera = "login"
+    reg = 'Регистрация'
+    rega = "reg"
+    return flask.render_template("index.html", login="anonim", enter=enter, entera=entera, rega=rega, reg=reg, lk=lk,
+                                 lka=lka)
 
 
 @app.route('/reg', methods=["POST", "GET"])
@@ -15,16 +38,21 @@ def reg():
         password = flask.request.form.get("person_pass")
         school = flask.request.form.get("school")
         type_student = flask.request.form.get("student")
+        cursor.execute('select * from users where nick="'+nick+'"')
+        ident = cursor.fetchall()
+        if ident:
+            return flask.render_template("reg.html", nick_reg="Такой логин уже занят!")
         if type_student == "on":
             type_user = 0
         else:
             type_user = 1
-        sql = 'insert into users values("' + fio + '", "' + nick + '", "' + password + '", "' + school + '", ' + str(
-            type_user) + ', "' + school + '", 0)'
+        sql = 'insert into users values("' + fio + '", "' + nick + '", "' + password + '", "' + school + '", ' + str(type_user) + ', "' + school + '", 0)'
         print(sql)
         cursor.execute(sql)
         conn.commit()
-    return flask.render_template("reg.html")
+        flask.session["user"] = nick
+        return session_render_index()
+    return flask.render_template("reg.html", nick_reg="Логин")
 
 
 @app.route('/login', methods=["POST", "GET"])
@@ -34,12 +62,31 @@ def login():
         cursor = conn.cursor()
         login = flask.request.form.get("login")
         password = flask.request.form.get("password")
-        cursor.execute('select * from users where nick="'+login+'" and password="'+password+'"')
+        cursor.execute('select * from users where nick="' + login + '" and password="' + password + '"')
         res = cursor.fetchall()
         if res:
-            flask.render_template("login.html")
-    return flask.render_template("login.html")
+            if not "user" in flask.session:
+                flask.session["user"] = login
+            return session_render_index()
+        else:
+            return flask.render_template("login.html", message="Данные не верны!")
+    return flask.render_template("login.html", message="")
+
+
+@app.route('/')
+def index():
+    if "user" in flask.session:
+        return session_render_index()
+    return anonim_render_index()
+
+
+@app.route('/exit')
+def exit():
+    if "user" in flask.session:
+        flask.session.pop('user', None)
+    return anonim_render_index()
 
 
 if __name__ == '__main__':
+    app.secret_key = "P@ssw0rd"
     app.run()
