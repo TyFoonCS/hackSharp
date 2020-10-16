e501d84ae4a35d0bbf39a6264d6469b07b10c6f5
Timur Galeev <templay3000@gmail.com>
2020-10-16 20:18:14 +0500

    Добавил логинку с БД

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import flask
 import sqlite3
+import json
 
 app = flask.Flask(__name__)
-
 
 
 @app.route('/reg', methods=["POST", "GET"])
@@ -19,11 +19,26 @@ def reg():
             type_user = 0
         else:
             type_user = 1
-        sql = 'insert into users values("'+fio+'", "'+nick+'", "'+password+'", "'+school+'", '+str(type_user)+', "'+school+'", 0)'
+        sql = 'insert into users values("' + fio + '", "' + nick + '", "' + password + '", "' + school + '", ' + str(
+            type_user) + ', "' + school + '", 0)'
         print(sql)
         cursor.execute(sql)
         conn.commit()
     return flask.render_template("reg.html")
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if flask.request.method == "POST":
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
+        login = flask.request.form.get("login")
+        password = flask.request.form.get("password")
+        cursor.execute('select * from users where nick="'+login+'" and password="'+password+'"')
+        res = cursor.fetchall()
+        if res:
+            flask.render_template("login.html")
+    return flask.render_template("login.html")
 
 
 if __name__ == '__main__':
