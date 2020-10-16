1494124c474c7a9a6cf98c86adabe3a0b6d1d8af
Timur Galeev <templay3000@gmail.com>
2020-10-16 14:16:15 +0500

    перввичный бэк

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/reg', methods=["POST", "GET"])
-def index():
-    return render_template("reg.html")
+def reg():
+    if flask.request.method == "POST":
+        fio = flask.request.form.get("person_name")
+        print(fio)
+    return flask.render_template("reg.html")
 
 
 if __name__ == '__main__':
